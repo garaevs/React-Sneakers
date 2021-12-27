0510fe5c185285eb03d6bfe72feedcb2d5ffc0af
Ruslan Garaev <r.garaevs@gmail.com>
2021-12-27 23:35:50 +0300

    Урок № 3. Создание компонентов React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Card from './components/Card';
 import Drawer from './components/Drawer';
 import Header from './components/Header';
 
+const arr = [
+	{
+		title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+		price: 12999,
+		imageUrl: '/img/sneakers/1.jpg',
+	},
+	{
+		title: 'Мужские Кроссовки Nike Air Max 270',
+		price: 12600,
+		imageUrl: '/img/sneakers/2.jpg',
+	},
+	{
+		title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+		price: 8499,
+		imageUrl: '/img/sneakers/3.jpg',
+	},
+	{
+		title: 'Кроссовки Puma X Aka Boku Future Rider',
+		price: 8999,
+		imageUrl: '/img/sneakers/4.jpg',
+	},
+];
+
 function App() {
 	return (
 		<div className='wrapper clear'>
@@ -16,10 +39,14 @@ function App() {
 					</div>
 				</div>
 				<div className='d-flex'>
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+					{arr.map(obj => (
+						<Card
+							title={obj.title}
+							price={obj.price}
+							imageUrl={obj.imageUrl}
+							onClick={() => console.log(123)}
+						/>
+					))}
 				</div>
 			</div>
 		</div>
